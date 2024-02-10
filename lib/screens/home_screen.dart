f1cf6abd8052f5d1bb1327f3cf127c2def1cfdc4
berinackr <bernacakir888@gmail.com>
2024-02-10 13:25:05 +0300

    completed search bar and some texts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+//import 'package:gap/gap.dart';
 import 'package:ticket_booking_app/utils/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,6 +17,7 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -22,7 +26,8 @@ class HomeScreen extends StatelessWidget {
                       children: [
                         Text(
                           "Good Morning", style: Styles.headLineStyle3,
-                          ),
+                        ),
+                        const Gap(5),  // SizedBox(height: 5,), bunun yerine Gap pack. ekledik
                         Text(
                           "Book Tickets", style: Styles.headLineStyle1,
                         ),
@@ -41,6 +46,36 @@ class HomeScreen extends StatelessWidget {
                           )
                         )
                       ),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child:Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      )
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                Row(
+                  children: [
+                    Text("Upcoming Flights", style: Styles.headLineStyle2,),
+                    // InkWell ile View all yazisi tiklanabilir hale geldi
+                    InkWell(
+                      onTap: () {
+                        print("you are tapped");
+                      },
+                      child: Text("View all", style: Styles.textStyle.copyWith(color: Styles.primaryColor),)
                     )
                   ],
                 )
